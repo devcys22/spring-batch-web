8da3aa5aaa3cabb24cfce3e328b4e30176b1a5cc
dev22 <ysoli36512@gmail.com>
2024-01-12 13:09:58 +0900

    first commit

---
+++ src/main/java/com/fastcampus/springbatchweb/SpringbatchWebApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.springbatchweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbatchWebApplication.class, args);
+    }
+
+}
